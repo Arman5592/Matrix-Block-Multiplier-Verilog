6946283fc55d6e3a3f9e4bf7b60119ce1059ba97
Arman5592 <soleimaniarman98@gmail.com>
2021-07-10 16:07:56 +0430

    utilized synthesizable addition units

--- Project/multiplier_top_level/accumulator.v
+++ Project/multiplier_top_level/accumulator.v
@@ -19,9 +19,8 @@ parameter s_wait = 3'b100;
 
 reg [2:0] state=s_idle;
 
-reg add_ack=0;
 wire add_ready;
-wire add_ready11,add_ready12,add_ready21,add_ready22;
+wire [3:0] add_readies;
 wire [31:0] result11,result12,result21,result22;
 reg [31:0] acc11=0,acc12=0,acc21=0,acc22=0;
 reg [31:0] a11,a12,a21,a22;
@@ -30,26 +29,25 @@ assign o_a12 = acc12;
 assign o_a21 = acc21;
 assign o_a22 = acc22;
 
-assign add_ready = add_ready11 & add_ready12 & add_ready21 & add_ready22;
+assign add_ready = & add_readies;
 
 reg add_reset = 1;
-reg add_load = 0;
+
+
+reg a_stb,b_stb,z_ack;
 
 always @(posedge clk) begin
     
     if(reset)begin
         state <= s_reset;
-        add_reset <= 0;
-        add_load <= 0;
-		  
+        add_reset <= 1;
     end
 	 else begin
 		 case (state)
 			  s_idle: begin
 					done <= 0;
-					add_ack <= 0;
-					add_reset <= 0;
-					add_load <= 0;
+					z_ack <= 0;
+					add_reset <= 1;
 					
 					if (start) begin
 						 state <= s_setup;
@@ -57,23 +55,28 @@ always @(posedge clk) begin
 						 a12 <= i_a12; 
 						 a21 <= i_a21; 
 						 a22 <= i_a22; 
+						 add_reset <= 1;
 					end
 			  end
 			  s_setup: begin
 						 state <= s_add;
-						 add_ack <= 1;
-						 add_load <= 1;
-						 add_reset <= 1;
+						 a_stb <= 1;
+                         b_stb <= 1;
+                         z_ack <= 1;
+						 add_reset <= 0;
 			  end
 			  s_add: begin
-					add_ack <= 0;
-					acc11 <= result11;
-					acc12 <= result12;
-					acc21 <= result21;
-					acc22 <= result22;
+					z_ack <= 0;
 					if (add_ready) begin
-						 state <= s_wait; 
-						 done <= 1;
+						acc11 <= result11;
+					    acc12 <= result12;
+					    acc21 <= result21;
+					    acc22 <= result22;
+						state <= s_wait; 
+						done <= 1;
+						a_stb <= 0;
+                        b_stb <= 0;
+                        z_ack <= 0;
 					end
 			  end
 			  s_wait: begin
@@ -92,47 +95,89 @@ end
 
 
 
-
-adder adder_acc11(
+adder addder_acc11(
     .clk(clk),
-    .reset(add_reset),
-    .load(add_load),
-    .Number1(acc11),
-    .Number2(a11),
-    .result_ack(add_ack),
-    .Result(result11),
-    .result_ready(add_ready11)
+    .rst(add_reset),
+    .input_a(acc11),
+    .input_b(a11),
+    .output_z(result11),
+    .output_z_stb(add_readies[0]),
+    .input_a_stb(a_stb),
+    .input_b_stb(b_stb),
+    .output_z_ack(z_ack)
 );
-adder adder_acc12(
+adder addder_acc12(
     .clk(clk),
-    .reset(add_reset),
-    .load(add_load),
-    .Number1(acc12),
-    .Number2(a12),
-    .result_ack(add_ack),
-    .Result(result12),
-    .result_ready(add_ready12)
+    .rst(add_reset),
+    .input_a(acc12),
+    .input_b(a12),
+    .output_z(result12),
+    .output_z_stb(add_readies[1]),
+    .input_a_stb(a_stb),
+    .input_b_stb(b_stb),
+    .output_z_ack(z_ack)
 );
-adder adder_acc21(
+adder addder_acc21(
     .clk(clk),
-    .reset(add_reset),
-    .load(add_load),
-    .Number1(acc21),
-    .Number2(a21),
-    .result_ack(add_ack),
-    .Result(result21),
-    .result_ready(add_ready21)
+    .rst(add_reset),
+    .input_a(acc21),
+    .input_b(a21),
+    .output_z(result21),
+    .output_z_stb(add_readies[2]),
+    .input_a_stb(a_stb),
+    .input_b_stb(b_stb),
+    .output_z_ack(z_ack)
 );
-adder adder_acc22(
+adder addder_acc22(
     .clk(clk),
-    .reset(add_reset),
-    .load(add_load),
-    .Number1(acc22),
-    .Number2(a22),
-    .result_ack(add_ack),
-    .Result(result22),
-    .result_ready(add_ready22)
+    .rst(add_reset),
+    .input_a(acc22),
+    .input_b(a22),
+    .output_z(result22),
+    .output_z_stb(add_readies[3]),
+    .input_a_stb(a_stb),
+    .input_b_stb(b_stb),
+    .output_z_ack(z_ack)
 );
 
 endmodule
 
+module test();
+
+reg [31:0] a11,a12,a21,a22;
+reg clk=0;
+reg start;
+reg reset = 0;
+wire [31:0]  acc11,acc12,acc21,acc22;
+wire done;
+accumulator acc(
+.i_a11(a11),
+.i_a12(a12),
+.i_a21(a21),
+.i_a22(a22),
+.clk(clk),
+.reset(reset),
+.start(start),
+.o_a11(acc11),
+.o_a12(acc12),
+.o_a21(acc21),
+.o_a22(acc22),
+.done(done)
+);
+  always #10 clk=~clk;  // 25MHz
+initial 
+ $monitor ("%0t acc11=%h acc12=%h acc21=%h acc22=%h a11=%h a12=%h a21=%h a22=%h state=%d ready_add=%b done=%b",$time, acc11,acc12,acc21,acc22,a11,a12,a21,a22,acc.state,acc.add_ready,done);
+
+initial begin
+    start = 1;
+    a11 = 32'h41f0f5c3;  //30.12
+    a12 = 32'h42ee999a;  //119.3
+    a21 = 32'h3ee66666;  //0.45
+    a22 = 32'h4158a3d7;  //13.54
+    #400
+ 
+    start = 1;
+    #1000
+    $finish;
+end
+endmodule
--- Project/multiplier_top_level/base_matrix_multiplier.v
+++ Project/multiplier_top_level/base_matrix_multiplier.v
@@ -24,11 +24,12 @@ wire a_ack,b_ack,z_stb;
 wire z_stb1,z_stb2,z_stb3,z_stb4,z_stb5,z_stb6,z_stb7,z_stb8;
 reg a_stb,b_stb,z_ack,rst=1;
 
-reg add_ack=0;
+reg a_stb_adder,b_stb_adder,z_ack_adder;
+
 reg add_reset=1;
-reg add_load=0;
-wire add_ready11,add_ready12,add_ready21,add_ready22;
-assign add_ready = add_ready11 & add_ready12 & add_ready21 & add_ready22;
+
+wire [3:0] add_readies;
+assign add_ready = &add_readies;
 assign z_stb = z_stb1 & z_stb2 & z_stb3 & z_stb4 & z_stb5 & z_stb6 & z_stb7 & z_stb8;
 
 reg [31:0] r_a11,r_a12,r_a21,r_a22,r_b11,r_b12,r_b21,r_b22;
@@ -38,9 +39,8 @@ always @(posedge clk) begin
         s_idle: begin
             done <= 0;
             rst <= 1;
-            add_ack <= 0;
-            add_reset <= 0;
-            add_load <= 0;
+            add_reset <= 1;
+            z_ack_adder <= 0;
             if (start) begin
                 state <= s_setup;
                 r_a11 <= i_a11;
@@ -65,20 +65,24 @@ always @(posedge clk) begin
             z_ack = 0;
             if(z_stb == 1) begin
                 state <= s_add;
-                add_ack <= 1;
-                add_load <= 1;
                 a_stb <= 0;
                 b_stb <= 0;
                 z_ack <= 0;
+
+                a_stb_adder <= 1;
+                b_stb_adder <= 1;
+                z_ack_adder <=1;
+                add_reset <= 0;
             end
         end
         s_add: begin
-            add_reset <= 1;
-            add_ack <= 0;
-            add_load <= 0;
+            z_ack_adder <= 0;
             if (add_ready) begin
             state <= s_idle;
             done <= 1;
+            a_stb_adder <= 0;
+            b_stb_adder <= 0;
+            z_ack_adder <= 0;
             end
         end
     endcase     
@@ -191,102 +195,106 @@ single_multiplier multiplier_a22_b22(
 
 adder adder_c11(
     .clk(clk),
-    .reset(add_reset),
-    .load(add_load),
-    .Number1(mult_a11_b11),
-    .Number2(mult_a12_b21),
-    .result_ack(add_ack),
-    .Result(o_c11),
-    .result_ready(add_ready11)
+    .rst(add_reset),
+    .input_a(mult_a11_b11),
+    .input_b(mult_a12_b21),
+    .output_z(o_c11),
+    .output_z_stb(add_readies[0]),
+    .input_a_stb(a_stb_adder),
+    .input_b_stb(b_stb_adder),
+    .output_z_ack(z_ack_adder)
 );
 
 adder adder_c12(
     .clk(clk),
-    .reset(add_reset),
-    .load(add_load),
-    .Number1(mult_a11_b12),
-    .Number2(mult_a12_b22),
-    .result_ack(add_ack),
-    .Result(o_c12),
-    .result_ready(add_ready12)
+    .rst(add_reset),
+    .input_a(mult_a11_b12),
+    .input_b(mult_a12_b22),
+    .output_z(o_c12),
+    .output_z_stb(add_readies[1]),
+    .input_a_stb(a_stb_adder),
+    .input_b_stb(b_stb_adder),
+    .output_z_ack(z_ack_adder)
 );
 
 adder adder_c21(
     .clk(clk),
-    .reset(add_reset),
-    .load(add_load),
-    .Number1(mult_a21_b11),
-    .Number2(mult_a22_b21),
-    .result_ack(add_ack),
-    .Result(o_c21),
-    .result_ready(add_ready21)
+    .rst(add_reset),
+    .input_a(mult_a21_b11),
+    .input_b(mult_a22_b21),
+    .output_z(o_c21),
+    .output_z_stb(add_readies[2]),
+    .input_a_stb(a_stb_adder),
+    .input_b_stb(b_stb_adder),
+    .output_z_ack(z_ack_adder)
 );
 adder addder_c22(
     .clk(clk),
-    .reset(add_reset),
-    .load(add_load),
-    .Number1(mult_a21_b12),
-    .Number2(mult_a22_b22),
-    .result_ack(add_ack),
-    .Result(o_c22),
-    .result_ready(add_ready22)
+    .rst(add_reset),
+    .input_a(mult_a21_b12),
+    .input_b(mult_a22_b22),
+    .output_z(o_c22),
+    .output_z_stb(add_readies[3]),
+    .input_a_stb(a_stb_adder),
+    .input_b_stb(b_stb_adder),
+    .output_z_ack(z_ack_adder)
 );
 
 endmodule
-//
-//module test();
-//
-//reg [31:0] a11,a12,a21,a22,b11,b12,b21,b22;
-//reg clk=0;
-//reg start;
-//wire [31:0]  c11,c12,c21,c22;
-//wire done;
-//matirx_multiplier mult(
-//.i_a11(a11),
-//.i_a12(a12),
-//.i_a21(a21),
-//.i_a22(a22),
-//.i_b11(b11),
-//.i_b12(b12),
-//.i_b21(b21),
-//.i_b22(b22),
-//.clk(clk),
-//.start(start),
-//.o_c11(c11),
-//.o_c12(c12),
-//.o_c21(c21),
-//.o_c22(c22),
-//.done(done)
-//);
-//  always #10 clk=~clk;  // 25MHz
-//initial 
-// $monitor ("%0t %h c11=%h c12=%h c21=%h c22=%h state: %d z_stb: %b add_ready: %b done: %b",$time,mult.mult_a11_b11,c11,c12,c21,c22,mult.state,mult.z_stb,mult.add_ready,mult.done);
-//
-//initial begin
-//    #10
-//    start = 1;
-//    a11 = 32'h41f0f5c3;  //30.12
-//    a12 = 32'h42ee999a;  //119.3
-//    a21 = 32'h3ee66666;  //0.45
-//    a22 = 32'h4158a3d7;  //13.54
-//    b11 = 32'h42b6ae14;  //91.34
-//    b12 = 32'h40400000;  //3
-//    b21 = 32'h41000000;  //8
-//    b22 = 32'h41780000;  //15.5
-//    // c11=3705.5608    0x456798f8 
-//    // c12=1939.51      0x44f27050
-//    // c21=149.423      0x43156c49
-//    // c22=211.22       0x43533850
-//    #10
-//    a11 = 32'h41f882f;  //30.12
-//    a12 = 32'h42ee999a;  //119.3
-//    a21 = 32'h3ee66666;  //0.45
-//    a22 = 32'h4158a3d7;  //13.54
-//    b11 = 32'h42b6a014;  //91.34
-//    b12 = 32'h40400100;  //3
-//    b21 = 32'h41010000;  //8
-//    b22 = 32'h41780001;  //15.5
-//    #650
-//    $finish;
-//end
-//endmodule
+
+module test();
+
+reg [31:0] a11,a12,a21,a22,b11,b12,b21,b22;
+reg clk=0;
+reg start;
+wire [31:0]  c11,c12,c21,c22;
+wire done;
+base_matrix_multiplier mult(
+.i_a11(a11),
+.i_a12(a12),
+.i_a21(a21),
+.i_a22(a22),
+.i_b11(b11),
+.i_b12(b12),
+.i_b21(b21),
+.i_b22(b22),
+.clk(clk),
+.start(start),
+.o_c11(c11),
+.o_c12(c12),
+.o_c21(c21),
+.o_c22(c22),
+.done(done)
+);
+ always #10 clk=~clk;  // 25MHz
+initial 
+$monitor ("%0t %h c11=%h c12=%h c21=%h c22=%h state: %d z_stb: %b add_ready: %b done: %b %b",$time,mult.mult_a11_b11,c11,c12,c21,c22,mult.state,mult.z_stb,mult.add_ready,mult.done,mult.add_readies);
+
+initial begin
+   #10
+   start = 1;
+   a11 = 32'h41f0f5c3;  //30.12
+   a12 = 32'h42ee999a;  //119.3
+   a21 = 32'h3ee66666;  //0.45
+   a22 = 32'h4158a3d7;  //13.54
+   b11 = 32'h42b6ae14;  //91.34
+   b12 = 32'h40400000;  //3
+   b21 = 32'h41000000;  //8
+   b22 = 32'h41780000;  //15.5
+   // c11=3705.5608    0x456798f8 
+   // c12=1939.51      0x44f27050
+   // c21=149.423      0x43156c49
+   // c22=211.22       0x43533850
+   #10
+   a11 = 32'h41f882f;  //30.12
+   a12 = 32'h42ee999a;  //119.3
+   a21 = 32'h3ee66666;  //0.45
+   a22 = 32'h4158a3d7;  //13.54
+   b11 = 32'h42b6a014;  //91.34
+   b12 = 32'h40400100;  //3
+   b21 = 32'h41010000;  //8
+   b22 = 32'h41780001;  //15.5
+   #5000
+   $finish;
+end
+endmodule
